8e1c74d8f8cde38d9671244302246ed7e9791fc5
Sihan Li <lisihan969@gmail.com>
2018-04-21 14:57:57 -0700

    Update data.

--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-python3 fetcher.py
-python3 render.py
-git commit -am 'Update data.'
-git push
+python3 fetcher.py && python3 render.py && git commit -am 'Update data.' && git push
